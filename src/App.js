099ed45b2ca420b0ecb8776593b93f9c83f04277
Jake Landgraf <landgrafjacob@gmail.com>
2022-03-24 00:49:29 -0400

    add result

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Entry from './components/Entry'
 
 function App() {
   const [ formData, setFormData ] = useState("slate")
+  const [ feedbackData, setFeedbackData ] = useState("00000")
 
   return (
     <div className="App">
       <Header />
-      <Entry formData={formData} setFormData={setFormData} />
+      <Entry formData={formData} setFormData={setFormData} feedbackData={feedbackData} setFeedbackData={setFeedbackData} />
     </div>
   );
 }
--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,20 +1,66 @@
 import React, {useState} from 'react';
 
-const Entry = ({ formData, setFormData }) => {
+const Entry = ({ formData, setFormData, feedbackData, setFeedbackData }) => {
     const [ guessed, setGuessed ] = useState(false)
-    function handleSubmit(event) {
+    function handleFirstSubmit(event) {
         event.preventDefault();
-        setGuessed(true)
+        setGuessed(true);
     }
+
+    function handleSecondSubmit(event) {
+        event.preventDefault();
+        console.log(feedbackData)
+    }
+
+    function handleButtonClick(event) {
+        const color = event.target.style.backgroundColor;
+        const id = parseInt(event.target.id);
+
+        switch (color) {
+            case "grey":
+                event.target.style.backgroundColor = "rgb(228, 208, 10)";
+                setFeedbackData(feedbackData.substr(0,id) + "y" + feedbackData.substr(id+1));
+                break;
+            case "rgb(228, 208, 10)":
+                event.target.style.backgroundColor = "green";
+                setFeedbackData(feedbackData.substr(0,id) + "g" + feedbackData.substr(id+1));
+                break;
+            default:
+                event.target.style.backgroundColor = "grey";
+                setFeedbackData(feedbackData.substr(0,id) + "0" + feedbackData.substr(id+1));
+        }
+    }
+
+    function handleBackClick() {
+        setGuessed(false);
+        setFeedbackData("00000");
+    }
+
+
     return (
-        guessed ?
-            <div>Guess: {formData}</div>:
-            
-            <form className="entry" onSubmit={handleSubmit}>
-                Input your guess: <input type="text" value={formData} onChange={e => setFormData(e.target.value)} />
-                <input type="submit" value="Enter" />
-            </form>
-            
+        <div>
+            {guessed ?
+                <div className="nobr">
+                    <form className="entry" onSubmit={handleSecondSubmit}>
+                    Input result: 
+                    {formData
+                        .split('')
+                        .map((c,i) => 
+                            <input type="button" key={i} id={i} value={c} onClick={handleButtonClick} style={{backgroundColor: 'grey'}}/>
+                        )}
+
+                    <input type="submit" value="Submit" />
+                    <button value="Back" onClick={handleBackClick}>Back</button>
+                    </form>
+                </div>
+                :
+                <form className="entry" onSubmit={handleFirstSubmit}>
+                    Input your guess: <input type="text" value={formData} onChange={e => setFormData(e.target.value)} />
+                    <input type="submit" value="Enter" />
+                </form>
+
+            }
+        </div>
     )
 }
 
